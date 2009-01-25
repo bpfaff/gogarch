527739772509cfa65f61ac1a59f24a2962c24002
Bernhard Pfaff <bernhard@pfaffikus.de>
2009-01-25 13:20:02 +0000

    replaced stop() with print() in validate function for Orthom.

--- R/Orthom-class.R
+++ R/Orthom-class.R
@@ -6,17 +6,17 @@ validOrthomObject <- function(object){
   if(all.equal(diff(dim(object@M)), 0, check.attributes = FALSE)){
     TRUE
   } else {
-    stop("\nObject is not a square matrix.\n")
+    print("\nObject is not a square matrix.\n")
   }
   if(all.equal(det(object@M), 1, check.attributes = FALSE)){
     TRUE
   } else {
-    stop("\nDeterminant of object is not equal to 1.\n")
+    print("\nDeterminant of object is not equal to 1.\n")
   }
   if(all.equal(crossprod(object@M), diag(m), check.attributes = FALSE)){
     TRUE
   } else {
-    stop("\nThe cross product of the object is not the Identity matrix.\n")
+    print("\nThe cross product of the object is not the Identity matrix.\n")
   }
 }
 ##
