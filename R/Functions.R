c8a2ff45b683c622eb1b59ba4ebb7bd8336b9a70
Bernhard Pfaff <bernhard@pfaffikus.de>
2009-02-18 20:50:28 +0000

    Changed argument in gogarch(), see ChangeLog.

--- R/Functions.R
+++ R/Functions.R
@@ -19,24 +19,24 @@
 ## gogarch: main function for estimating GO-GARCH models
 ##
 gogarch <-
-function(data, formula, scale = FALSE, method = c("ica", "mm", "ml", "nls"), lag.max = 1, initial = NULL, garchlist = list(init.rec = "mci", delta = 2, skew = 1, shape = 4, cond.dist = "norm", include.mean = FALSE, include.delta = NULL, include.skew = NULL, include.shape = NULL, leverage = NULL, trace = FALSE, algorithm = "nlminb", hessian = "ropt", control = list(), title = NULL, description = NULL), ...){
-  method <- match.arg(method)
+function(data, formula, scale = FALSE, estby = c("ica", "mm", "ml", "nls"), lag.max = 1, initial = NULL, garchlist = list(init.rec = "mci", delta = 2, skew = 1, shape = 4, cond.dist = "norm", include.mean = FALSE, include.delta = NULL, include.skew = NULL, include.shape = NULL, leverage = NULL, trace = FALSE, algorithm = "nlminb", hessian = "ropt", control = list(), title = NULL, description = NULL), ...){
+  estby <- match.arg(estby)
   Call <- match.call()
   gini <- goinit(X = data, garchf = formula, scale = scale)
   gomod <- new("GoGARCH", gini)
-  if(method == "ml"){
+  if(estby == "ml"){
     goestml <- new("Goestml", gomod)
     gogarch <- goest(object = goestml, initial = initial, garchlist = garchlist, ...)
   }
-  if(method == "nls"){
+  if(estby == "nls"){
     goestnls <- new("Goestnls", gomod)
     gogarch <- goest(object = goestnls, initial = initial, garchlist = garchlist, ...)
   }
-  if(method == "mm"){
+  if(estby == "mm"){
     goestmm <- new("Goestmm", gomod)
     gogarch <- goest(object = goestmm, lag.max = lag.max, garchlist = garchlist, ...)
   }  
-  if(method == "ica"){
+  if(estby == "ica"){
     goestica <- new("Goestica", gomod)
     gogarch <- goest(object = goestica, initial = initial, garchlist = garchlist, ...)
   }
